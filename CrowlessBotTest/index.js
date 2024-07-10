54b32d3c719dc72f5171a946ac0e2a5c527b0205
Jonnathan G <138323611+jonnathan4rt@users.noreply.github.com>
2024-07-09 19:24:05 -0500

    Update index.js

--- CrowlessBotTest/index.js
+++ CrowlessBotTest/index.js
@@ -1,4 +1,6 @@
 const { Telegraf } = require('telegraf');
+
+// Importar comandos
 const banCommand = require('./commands/admin-command/ban');
 const infoCommand = require('./commands/users-command/info');
 const helpCommand = require('./commands/bot-command/help');
@@ -11,46 +13,28 @@ const antiSpamCommand = require('./commands/general-command/anti-spam');
 const muteCommand = require('./commands/admin-command/mute');
 const unmuteCommand = require('./commands/admin-command/unmute');
 const helloCommand = require('./commands/general-command/hello');
-const http = require('http');
 
+// Inicializar el bot con el token
 const token = '7301985658:AAEsz-N9Buoau-LnY-h9mV0M94OF7MwpBpw';
 const bot = new Telegraf(token);
 
-// General Command
-antiSpamCommand(bot);
-helloCommand(bot);
-
-// Bot Command
-startCommand(bot);
-helpCommand(bot);
-
-// Users Command
-infoCommand(bot);
-staffCommand(bot);
-reportCommand(bot);
-
-// ADMIN Command
+// Registrar comandos del bot
 banCommand(bot);
+infoCommand(bot);
+helpCommand(bot);
+startCommand(bot);
+staffCommand(bot);
 repeatCommand(bot);
+reportCommand(bot);
 surjanCommand(bot);
+antiSpamCommand(bot);
 muteCommand(bot);
 unmuteCommand(bot);
+helloCommand(bot);
 
 // Iniciar el bot de Telegram
 bot.launch().then(() => {
   console.log('Bot iniciado correctamente.');
 }).catch((err) => {
   console.error('Error al iniciar el bot:', err);
-});
-
-// Configuración del servidor ficticio para Render
-const port = process.env.PORT || 3000;
-
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Bot en funcionamiento\n');
-});
-
-server.listen(port, () => {
-  console.log(`Servidor ficticio escuchando en el puerto ${port}`);
 });
